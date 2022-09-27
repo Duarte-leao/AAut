f03b4555188023c8e3b3bcb5eb64931a6ac74516
Duarte <duarte3391@gmail.com>
2022-09-27 13:09:28 +0100

    'duarte'

--- Projeto/Regression task 1/R_task1.py
+++ Projeto/Regression task 1/R_task1.py
@@ -1,4 +1,4 @@
-from re import X
+
 import numpy as np
 from sklearn import linear_model as lm
 
@@ -17,6 +17,8 @@ reg.fit(Xtrain,Ytrain)
 
 Ypred = reg.predict(Xval)
 
+SSE 
 
-print(reg.coef_)
-print(reg.score(Xtrain,Ytrain))
+
+""" print(reg.coef_)
+print(reg.score(Xtrain,Ytrain)) """
