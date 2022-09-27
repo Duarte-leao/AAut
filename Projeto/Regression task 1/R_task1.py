bc3293fee3c60d351e8c96bd159de16e9d208e3d
Duarte <duarte3391@gmail.com>
2022-09-27 14:14:05 +0100

    'duarte'

--- Projeto/Regression task 1/R_task1.py
+++ Projeto/Regression task 1/R_task1.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from sklearn import linear_model as lm
 
@@ -6,18 +5,20 @@ from sklearn import linear_model as lm
 X = np.load('Xtrain_Regression1.npy')
 Y = np.load('Ytrain_Regression1.npy')
 
-Xtrain = X[:80,:]
-Ytrain = Y[:80,:]
+Xtrain = X[:95,:]
+Ytrain = Y[:95,:]
 
-Xval = X[80:,:]
-Yval = Y[80:,:]
+Xval = X[95:,:]
+Yval = Y[95:,:]
 
 reg = lm.LinearRegression()
 reg.fit(Xtrain,Ytrain)
 
 Ypred = reg.predict(Xval)
 
-SSE 
+SSE =  np.sum((Yval-Ypred)**2)
+print(SSE)
+
 
 
 """ print(reg.coef_)
