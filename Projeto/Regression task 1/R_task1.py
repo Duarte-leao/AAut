330ea316866e734bcdb510cd80de02711bc146d4
Duarte <duarte3391@gmail.com>
2022-10-02 19:16:54 +0100

    'duarte'

--- Projeto/Regression task 1/R_task1.py
+++ Projeto/Regression task 1/R_task1.py
@@ -23,13 +23,16 @@ X_test = np.load('Xtest_Regression1.npy')
 
 k = 10 # Number of splits in cross validation
 
+
+Regressions_names = []
 #### Ordinary Least Squares ####
+Regressions_names.append('Ordinary Least Squares Regression')
 
 lin_reg_scores = cross_validate(linear_model.LinearRegression(), X, Y, cv = k, scoring = 'neg_mean_squared_error', return_train_score=True) 
 lin_reg_MSE = abs(lin_reg_scores['test_score'].mean())
 
 #### Ridge Regression ####
-
+Regressions_names.append('Ridge Regression')
 # Generate array of alphas
 ridge_alphas = []
 alpha = 1
@@ -43,7 +46,7 @@ ridge_alpha = ridge_reg.alpha_ # Choose alpha that best fits the data
 ridge_reg_MSE = abs(ridge_reg.best_score_) # Score of the best alpha
 
 #### Lasso Regression ####
-
+Regressions_names.append('Lasso Regression')
 # Generate array of alphas
 lasso_alphas = []
 alpha = 1
@@ -60,7 +63,7 @@ lasso_reg_scores = cross_validate(linear_model.Lasso(alpha = lasso_alpha), X, Y,
 lasso_reg_MSE = abs(lasso_reg_scores['test_score'].mean())
 
 #### Elastic-Net Regression ####
-
+Regressions_names.append('Elastic-Net Regression')
 # Generate array of alphas and l1-ratios
 elastic_alphas = []
 elastic_l1_ratios = []
@@ -85,20 +88,41 @@ elastic_net_reg_scores = cross_validate(elastic_net, X, Y, cv = k, scoring = 'ne
 elastic_net_reg_MSE = abs(elastic_net_reg_scores['test_score'].mean())
 
 #### Least Angle Regression ####
+Regressions_names.append('Least Angle Regression')
 ### PERGUNTAR A PROFESSORA SE É SUPOSTA NORMALIZAR O DATASET ANTES DE USAR LARS/LARSLASSO/OMP
 
 LARS_reg_scores = cross_validate(linear_model.Lars(normalize = False), X, Y, cv = k, scoring = 'neg_mean_squared_error', return_train_score = True)
 LARS_reg_MSE = abs(LARS_reg_scores['test_score'].mean())
-print(LARS_reg_MSE)
+
+
+#### Lasso Least Angle Regression ####
+Regressions_names.append('Lasso Least Angle Regression')
+
+lassoLars_reg = linear_model.LassoLarsCV(cv = k, normalize=False).fit(X, np.ravel(Y))
+lassoLars_alpha = lassoLars_reg.alpha_ # Choose alpha that best fits the data
+
+lassoLars_reg_scores = cross_validate(linear_model.LassoLars(alpha = lassoLars_alpha, normalize=False), X, Y, cv = k, scoring = 'neg_mean_squared_error', return_train_score = True)
+lassoLars_reg_MSE = abs(lassoLars_reg_scores['test_score'].mean())
+
+#### Orthogonal Matching Pursuit Regression ####
+Regressions_names.append('Orthogonal Matching Pursuit Regression')
+
+OMP_reg = linear_model.OrthogonalMatchingPursuitCV(cv = k, normalize=True, max_iter=10).fit(X, np.ravel(Y))
+OMP_n_zero_coef = OMP_reg.n_nonzero_coefs_ # Choose number of non zero coefficients that best fits the data
+
+OMP_reg_scores = cross_validate(linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs = OMP_n_zero_coef, normalize=True), X, Y, cv = k, scoring = 'neg_mean_squared_error', return_train_score = True)
+OMP_reg_MSE = abs(OMP_reg_scores['test_score'].mean())
 
 
 #### Selection of best estimator to do prediction ####
 
-predictors = [linear_model.LinearRegression(), linear_model.Ridge(alpha = ridge_alpha), linear_model.Lasso(alpha = lasso_alpha), linear_model.ElasticNet(alpha = lasso_alpha, l1_ratio = elastic_l1_ratio), linear_model.Lars(normalize=False)]
-MSE_array = np.array([lin_reg_MSE, ridge_reg_MSE, lasso_reg_MSE, elastic_net_reg_MSE, LARS_reg_MSE])
+predictors = [linear_model.LinearRegression(), linear_model.Ridge(alpha = ridge_alpha), linear_model.Lasso(alpha = lasso_alpha), linear_model.ElasticNet(alpha = lasso_alpha, l1_ratio = elastic_l1_ratio), linear_model.Lars(normalize=False), linear_model.LassoLars(alpha = lassoLars_alpha, normalize=False), linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs = OMP_n_zero_coef, normalize=True)]
+MSE_array = np.array([lin_reg_MSE, ridge_reg_MSE, lasso_reg_MSE, elastic_net_reg_MSE, LARS_reg_MSE, lassoLars_reg_MSE, OMP_reg_MSE])
 min_MSE = np.argmin(MSE_array)
-# print(MSE_array)
-# print(min_MSE)
+
+print(MSE_array)
+print(min_MSE)
+print('Best Predictor: ', Regressions_names[min_MSE])
 
 predictor = predictors[min_MSE]
 
